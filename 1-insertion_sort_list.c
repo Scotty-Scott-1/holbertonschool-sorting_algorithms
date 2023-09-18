066380c53e7ef54461c50963074f79f0969cd869
GTDeschamps <g.deschamps@hotmail.fr>
2023-09-18 17:28:43 +0200

    creation Task 1 : insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+/**
+ *insertion_sort_list -sorted the list
+ * @list:linked list
+ *Return:void
+ */
+void insertion_sort_list(listint_t **list)
+{
+
+	listint_t *key = (*list)->next;
+	listint_t *insert = NULL;
+	listint_t *temp = NULL;
+
+	while (key != NULL)
+	{
+		insert = key->prev;
+		temp = key;
+
+		while (insert != NULL && insert->n > temp->n)
+		{
+			temp->prev = insert->prev;
+			insert->prev = temp;
+			if (temp->prev != NULL)
+			{
+				temp->prev->next = temp;
+			}
+			insert->next = temp->next;
+			temp->next = insert;
+
+			if (insert->next != NULL)
+			{
+				insert->next->prev = insert;
+			}
+			if (insert == *list)
+			{
+				*list = temp;
+			}
+			insert = temp->prev;
+			print_list(*list);
+		}
+		key = key->next;
+	}
+}
