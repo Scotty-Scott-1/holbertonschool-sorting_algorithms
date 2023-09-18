86b269272f18a39523024d9307a78f707bb9aae7
GTDeschamps <g.deschamps@hotmail.fr>
2023-09-18 15:36:25 +0200

    correction with betty for Task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,11 +12,11 @@ void bubble_sort(int *array, size_t size)
 	size_t j = 0;
 	int tmp = 0;
 
-	for(i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
-			if(array[j] > array[j + 1])
+			if (array[j] > array[j + 1])
 			{
 				tmp = array[j];
 				array[j] = array[j + 1];
