f66df7f1f81295422df2bba788e569d1df98288b
GTDeschamps <g.deschamps@hotmail.fr>
2023-09-18 16:14:24 +0200

    add Array == Null in 0-Bubble-sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,6 +12,9 @@ void bubble_sort(int *array, size_t size)
 	size_t j = 0;
 	int tmp = 0;
 
+	if (array == NULL || size < 2)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
