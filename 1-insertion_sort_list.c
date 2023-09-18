c13b809e1f59b8e0d9f86718ce09d14117730b3a
GTDeschamps <g.deschamps@hotmail.fr>
2023-09-18 17:40:04 +0200

    Correction of task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,7 +11,7 @@ void insertion_sort_list(listint_t **list)
 	listint_t *insert = NULL;
 	listint_t *temp = NULL;
 
-	if(list == NULL || (*list)->next == NULL)
+	if (*list == NULL || (*list)->next == NULL)
 		return;
 
 	while (key != NULL)
