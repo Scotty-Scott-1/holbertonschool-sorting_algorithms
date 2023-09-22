2eb85d234a0d006ed9a316051ce62f8de31af934
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-09-22 11:12:56 +0200

    updated sort.h

--- sort.h
+++ sort.h
@@ -2,6 +2,7 @@
 #define SORT_H
 
 #include <stdlib.h>
+#include <stdio.h>
 
 
 /**
@@ -24,5 +25,5 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
+void shell_sort(int *array, size_t size);
 #endif /* SORT_H */
